c78845a19fbc096b69f7b99e413b5662f22a1683
Pavittar Kumar Azad <56643175+PavittarKumar@users.noreply.github.com>
2020-10-02 00:21:28 +0530

    Update asgi.py

--- diet_suggestion_webapp/diet_suggestion_webapp/asgi.py
+++ diet_suggestion_webapp/diet_suggestion_webapp/asgi.py
@@ -3,7 +3,7 @@ ASGI config for diet_suggestion_webapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information please refer to the following link: 
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
